d525bf33783c17e83a34a8265b842de5f536f0cc
Adson Tanajura <adson.tanajura@gmail.com>
2023-05-30 17:29:51 -0300

    Schedule Registration

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
-import express, { request, response } from 'express';
-
+import express from "express";
+import routes from "./routes";
 const app = express();
 
-app.get('/', (request, response) =>{
-    const jsonResponse = { message: 'Hello,World'}
-    console.log(`response: ${JSON.stringify(jsonResponse)}`)
-    return response.json(jsonResponse);
-})
+app.use(express.json())
+
+app.use(routes);
 
 app.listen(3333, () => {
-    console.log('Server Started on port 3333!!!')
+    console.log('Server Started on port 3333!!!');
 });
