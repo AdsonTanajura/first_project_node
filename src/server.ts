6dc455cfb8d5ed2453097182e69c324dd1876694
Adson <adson.tanajura@gmail.com>
2024-07-30 07:52:24 -0300

    cria a rota files para poder ver as imagens

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
 import postgresDataSource from './datebase/data-source';
+import uplaodConfig from '../src/config/uplaod';
 const app = express();
 
 app.use(express.json());
-
+app.use('/files', express.static(uplaodConfig.directory));
 app.use(routes);
 
 postgresDataSource.initialize().then(async () =>{
