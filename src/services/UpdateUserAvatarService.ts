ff26a712d9f6f6a9d8e50c49be774786f7beb6ed
Adson <adson.tanajura@gmail.com>
2024-07-30 07:44:57 -0300

    Resolver o bug do caminho que nao existia

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -5,36 +5,35 @@ import path from 'path';
 import uplaodConfig from '../config/uplaod';
 import fs from 'fs';
 
-
 class UpdadeUserAvatarService {
     public async execute({ user_id, avatarFilename }: UpdateUserAvatarServiceDTO): Promise<User> {
-        const usersRepository =  postgresDataSource.getRepository(User);
+        const usersRepository = postgresDataSource.getRepository(User);
 
-
-        const user = await usersRepository.findOne({ 
-            where: { id: user_id } 
-        });
+        const user = await usersRepository.findOne({ where: { id: user_id } });
 
         if (!user) {
-            throw new Error('Only authenticated users can change avatar.')   
+            throw new Error('Only authenticated users can change avatar.');
         }
 
         if (user.avatar) {
-
             const userAvatarFilePath = path.join(uplaodConfig.directory, user.avatar);
-            const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
-            if (userAvatarFileExists) {
+            try {
+                await fs.promises.stat(userAvatarFilePath);
                 await fs.promises.unlink(userAvatarFilePath);
+            } catch (err: any) {
+                if (err.code !== 'ENOENT') {
+                    throw err;
+                }
             }
         }
+
         user.avatar = avatarFilename;
 
-        await usersRepository.save(user)
+        await usersRepository.save(user);
 
         return user;
     }
-
 }
 
 export default UpdadeUserAvatarService;
