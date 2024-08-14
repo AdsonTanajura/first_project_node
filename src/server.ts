d0d900076a798ce8f82cb5f3e42dd2d1d255851a
AdsonTanajura <adson.tanajura@gmail.com>
2024-08-13 21:16:32 -0300

    add cors on project

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 import postgresDataSource from './datebase/data-source';
 import uplaodConfig from '../src/config/uplaod';
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use('/files', express.static(uplaodConfig.directory));
 app.use(routes);
